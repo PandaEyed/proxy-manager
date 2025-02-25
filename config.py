392322d9f79d126572d4fe9c4333ab3c2e9ef364
yunchuan.wu <yunchuan523@gmail.com>
2025-02-25 17:18:29 +0800

    线上发布成功，使用 rds，v0.0.1

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://yunchuan:Blackeyeds911@localhost/xiaoming'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://frps:WapShuAsHUa328@rm-bp1332h32z2q71bgu0o.mysql.rds.aliyuncs.com/frps_supplier_manager'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
